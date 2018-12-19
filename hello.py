818d45dac5223e1148db1340703b08585e338e35
heimao0307 <fengyun86868@163.com>
2018-12-19 15:02:48 +0800

    Hello World,Hello GitHub.

---
+++ hello.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Auther   : 许杰辉
+
+
+"""
+老规矩：
+从 Hello World开始
+"""
+
+
+print("----------------------")
+print("Hello World!\nMy name is Jiehui Xu.")
+print("----------------------")
+print("Hello GitHub!\nNice to meet you.")
+print("----------------------")
+
+"""
+来一首喜欢的诗吧
+
+
+未 选 择 的 路 
+
+弗罗斯特 
+
+黄色的树林里分出两条路， 
+可惜我不能同时去涉足， 
+我在那路口久久伫立， 
+我向着一条路极目望去， 
+直到它消失在丛林深处。 
+但我却选了另外一条路， 
+它荒草萋萋，十分幽寂， 
+显得更诱人、更美丽， 
+虽然在这两条小路上， 
+都很少留下旅人的足迹， 
+虽然那天清晨落叶满地， 
+两条路都未经脚印污染。 
+呵，留下一条路等改日再见！ 
+但我知道路径延绵无尽头， 
+恐怕我难以再回返。 
+也许多少年后在某个地方， 
+我将轻声叹息把往事回顾， 
+一片树林里分出两条路， 
+而我选了人迹更少的一条， 
+从此决定了我一生的道路。 
+
+
+The Road Not Taken 
+
+ROBERT FROST 
+
+Two roads diverged in a yellow wood, 
+And sorry I could not travel both 
+And be one traveler, long I stood 
+And looked down one as far as I could 
+To where it bent in the undergrowth; 
+Then took the other, as just as fair, 
+And having perhaps the better claim, 
+Because it was grassy and wanted wear; 
+Though as for that the passing there 
+Had worn them really about the same, 
+And both that morning equally lay 
+In leaves no step had trodden black. 
+Oh, I kept the first for another day! 
+Yet knowing how way leads on to way, 
+I doubted if I should ever come back. 
+I shall be telling this with a sigh 
+Somewhere ages and ages hence: 
+Two roads diverged in a wood, and I- 
+I took the one less traveled by, 
+And that has made all the difference. 
+"""
